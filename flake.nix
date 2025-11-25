0c113d22927560fd2375b42a2fa403dfcb78549a
meir <michaelmeir1222001@email.com>
2025-11-25 17:11:00 +0100

    fix overlay

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
         }
       );
 
-      nixosModules.nix-fs = args: import ./nix/overlay.nix;
+      nixosModules.nix-fs = import ./nix/overlay.nix;
 
       devShells = eachSystem (pkgs: {
         default = pkgs.callPackage ./nix/shell.nix { inherit pre-commit-hooks pkgs; };
